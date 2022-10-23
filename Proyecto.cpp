0f747e1028548e369fed359cbf75c3dc3546a848
RikelmyVivas <114117430+RikelmyVivas@users.noreply.github.com>
2022-10-22 22:39:02 -0600

    Update Proyecto.cpp

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -1,33 +1,15 @@
-//librerias
 #include <iostream>
+
 // #include "./Functions.cpp"
 
 
-using namespace std;//Nombre de espacio
+using namespace std;
 
 
-//funcion principal
+
 int main () {
 
-//declaracion de variables
-int opcion;
-
-do{
-	cout<<"---------------------MENÚ----------------------"
-	cout<<"1. Ingresar una oferta laboral"<<endl;
-	cout<<"2. Eliminar una oferta laboral"<<endl;
-	cout<<"3. Mostrar las ofertas laborales"<<endl;
-	cout<<"4. Mostrar postulantes"<<endl;
-	cout<<"5. Mostrar reservaciones"<<endl;
-	cout<<"6. Buscar una oferta laboral, por medio del id"<<endl
-	cout<<"7. Salir"<<endl;
-	
-	if(opcion == 1){
-		
-	}
-	
-}while(opciones !=7);
-
+ hola mundo	
 
     return 0;
 }
