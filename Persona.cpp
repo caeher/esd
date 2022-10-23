a0097ed50b9572b1a9f9ba41e2920706dc92ee50
RikelmyVivas <114117430+RikelmyVivas@users.noreply.github.com>
2022-10-22 20:41:29 -0600

    Create Persona.cpp

---
+++ Persona.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string.h>
+
+main()
+{
+	struct Persona 
+	{
+	char nombre[50];
+ 	char apellidos[50];
+ 	char direccion[50];
+ 	char genero[10];
+ 	int edad;
+ 	
+};
+ 
